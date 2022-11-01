b2ef46bf451898702e7663283e3713db0c05ab1d
josceno <jdamasceno414@gmail.com>
2022-11-01 13:20:36 -0300

    java dicionario

---
+++ lessons/aula2/Desafio.java
@@ -0,0 +1,16 @@
+package lessons.aula2;
+
+public class Desafio {
+    
+    double preco;
+    double consumokml;
+    double distanciaPercorrer;
+
+    public static void main(String[] args){
+
+
+    }
+    public void calcularGasto(){
+
+    }
+}
