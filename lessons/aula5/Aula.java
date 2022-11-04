6cc095a966755b42dd668b60c1a710fb31dc8c9e
josceno <jdamasceno414@gmail.com>
2022-11-04 13:51:37 -0300

    adding a interface

--- lessons/aula5/Aula.java
+++ lessons/aula5/Aula.java
@@ -3,28 +3,48 @@ package lessons.aula5;
 public class Aula {
     public static void main(String[] args){
         Pessoa Jorge = new Pessoa("Jorge", 1.70, 70);
+
+        Jorge.getID(1);
         Jorge.calcularIMC();
     }
 }
-class Pessoa extends Ocupação{
+class Pessoa extends Medidas implements PessoaId{
     String nome;
     public Pessoa(String nome,double altura, double peso) {
         super(altura, peso);
         this.nome = nome;     
     }
     public void calcularIMC(){
-        double IMC = altura/peso;
+        double IMC = Peso()/Altura();
         System.out.println(IMC); 
+    }
+    @Override
+    public void getID(int id) {
+       System.out.println(id);
     }
 
 }
 
-abstract class Ocupação{
-    double altura;
-    double peso;
-    public Ocupação(double d,double peso){
-      
-        this.altura = d;
+abstract class Medidas{
+    private double altura;
+    private double peso;
+    public Medidas(double altura,double peso){     
+        this.altura = altura;
         this.peso = peso;
     }
+    public double Altura(){
+        return altura;
+    }
+    public void setaltura(double altura){
+        this.altura = altura;
+    }
+    public double Peso(){
+        return peso;
+    }
+    public void setpeso(double peso){
+        this.peso = peso;
+    }
+}
+interface PessoaId{
+    void getID(int id);
 }
